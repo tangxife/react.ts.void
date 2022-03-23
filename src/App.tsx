a219bdca6b993aa6f342ac68e75e989abd71febb
tangxife <tang.xf.33@icloud.com>
2022-03-23 14:15:18 +0800

    feture: add some graphql docments

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import { ApolloProvider } from "@apollo/client"
 import { ChakraProvider, Button } from "@chakra-ui/react"
 import Span from "src/components/Span"
 import ExchangeRates from "src/pages/ExchangeRates/ExchangeRates"
-import { useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next"
 
 import { useApollo } from "src/hooks/Apollo/useApollo"
 
 function App() {
-  const {t} = useTranslation()
+  const { t } = useTranslation()
   const { client } = useApollo()
 
   return (
