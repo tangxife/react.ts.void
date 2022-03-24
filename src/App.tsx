7a3803e0ad0aa7f81d13c3610c5986fb680409e3
tangxife <tang.xf.33@icloud.com>
2022-03-24 23:40:57 +0800

    fix: modify Text component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react"
 import { ApolloProvider } from "@apollo/client"
 import { ChakraProvider, Button } from "@chakra-ui/react"
-import Span from "./components/Span"
+import Text from "./components/Text"
 import ExchangeRates from "./pages/ExchangeRates/ExchangeRates"
+import theme from "./theme/theme"
 
 import { useApollo } from "./hooks/Apollo/useApollo"
 
@@ -14,14 +15,30 @@ function App() {
       <ChakraProvider>
         <div className="App">
           <Button colorScheme="blue">ChakraButton</Button>
-          <Span textStyle="h1">test text</Span>
-          <Span textStyle="h2">test text</Span>
-          <Span textStyle="h3">test text</Span>
-          <Span textStyle="h4">test text</Span>
-          <Span textStyle="h5">test text</Span>
-          <Span textStyle="h6">test text</Span>
-          <Span textStyle="h6">test text</Span>
-          <Span textStyle="md">test text</Span>
+          <Text fontStyle="h1" fontColor={theme.Color.Error.error} as="span">
+            test text
+          </Text>
+          <Text fontStyle="h2" fontColor={theme.Color.Error.error}>
+            test text
+          </Text>
+          <Text fontStyle="h3" fontColor={theme.Color.Error.error} as="span">
+            test text
+          </Text>
+          <Text fontStyle="h4" fontColor={theme.Color.Error.error} as="p">
+            test text
+          </Text>
+          <Text fontStyle="h5" fontColor={theme.Color.Text.text02} as="p">
+            test text
+          </Text>
+          <Text fontStyle="h6" fontColor={theme.Color.Text.text02} as="span">
+            test text
+          </Text>
+          <Text fontStyle="h6" fontColor={theme.Color.Text.text02} as="span">
+            test text
+          </Text>
+          <Text fontStyle="md" fontColor={theme.Color.Text.text02} as="span">
+            test text
+          </Text>
           <ExchangeRates />
         </div>
       </ChakraProvider>
