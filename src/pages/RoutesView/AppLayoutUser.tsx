2342709de23e4addc47e9bb2e39116f414a9e19a
tangxife <tang.xf.33@icloud.com>
2022-03-26 18:54:58 +0800

    refactor

--- src/pages/RoutesView/AppLayoutUser.tsx
+++ src/pages/RoutesView/AppLayoutUser.tsx
@@ -9,6 +9,7 @@ const AppLayoutUser: React.FC = () => {
     { key: "logout", menuTitle: "ログアウト" },
     { key: "other", menuTitle: "その他" }
   ]
+  const [pageName, setPageName] = useState<string>("")
   const [selectedTab, setSelectedTab] = useState<string>("home")
 
   const handleMenuSelect = (key: string) => {
@@ -16,7 +17,16 @@ const AppLayoutUser: React.FC = () => {
   }
 
   const haneleTabSelect = (key: string) => {
+    const pages = [
+      { key: "home", pageName: "" },
+      { key: "candidates", pageName: "候補者一覧" },
+      { key: "questions", pageName: "質問テンプレート一覧" },
+      { key: "information", pageName: "説明資料" },
+      { key: "settings", pageName: "設定" }
+    ]
+
     setSelectedTab(key)
+    setPageName(pages.find((p) => p.key === key)?.pageName ?? "")
     console.log("select tab: ", key)
   }
 
@@ -24,7 +34,7 @@ const AppLayoutUser: React.FC = () => {
     <Flex>
       <SideBar selectedTab={selectedTab} onTabSelect={haneleTabSelect} />
       <VStack mt="0px" justify="start">
-        <Header pageName="候補者一覧" userName={userName} menuItems={menuItems} onMenuSelect={handleMenuSelect} />
+        <Header pageName={pageName} userName={userName} menuItems={menuItems} onMenuSelect={handleMenuSelect} />
         <Box bg="blue">main</Box>
       </VStack>
     </Flex>
--- src/pages/RoutesView/index.tsx
+++ src/pages/RoutesView/index.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import AppLayoutUser from "./AppLayoutUser"
-import Candidate from "src/pages/User/Candidate"
+import Candidate from "src/pages/User/Candidates"
 
 const RoutesView = () => {
   return (
